6785dee94221d260f5c88e70a1ae088ac7e2ea58
seyed <rseyedmoosavi@gmail.com>
2021-08-02 12:18:38 +0430

    gh-pages

---
+++ precache-manifest.cca7c6a14abd80b7a85fb40b68ce8ba6.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "751aa4c071972114873c",
+    "url": "/css/app.4d08c71d.css"
+  },
+  {
+    "revision": "82b9c7a5a3f405032b1db71a25f67021",
+    "url": "/img/logo.82b9c7a5.png"
+  },
+  {
+    "revision": "ea9ed008074f632179a3d0c9e6c7f90f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "65e6c6eb3e7344fde001",
+    "url": "/js/about.a14fa351.js"
+  },
+  {
+    "revision": "751aa4c071972114873c",
+    "url": "/js/app.6bd13439.js"
+  },
+  {
+    "revision": "3ae62b26aa5fde858a19",
+    "url": "/js/chunk-vendors.ab2db9a6.js"
+  },
+  {
+    "revision": "2135eb691198c4eaaa62e2833e0cb6ec",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
